feb2a90c398c8cef190a8fd2102d6fafa7b8fc84
Shaun Broomfield <personal@shaunbroomfield.com>
2024-12-17 09:13:24 +0000

    Enhance chess game UI with dynamic board sizing and additional game information

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 function love.load()
     love.window.setTitle("Basic Chess Game")
-    love.window.setMode(800, 800)
+    love.window.setMode(800, 800, {resizable = true, minwidth = 400, minheight = 400})
     board = {}
     for i = 1, 8 do
         board[i] = {}
@@ -10,6 +10,8 @@ function love.load()
     end
     whiteColor = {1, 1, 1}
     blackColor = {0, 0, 0}
+    backgroundColor = {0, 0, 0}
+    borderColor = {0, 0, 0}
     function hexToRgb(hex)
         hex = hex:gsub("#", "")
         return {tonumber("0x" .. hex:sub(1, 2)) / 255, tonumber("0x" .. hex:sub(3, 4)) / 255, tonumber("0x" .. hex:sub(5, 6)) / 255}
@@ -20,37 +22,58 @@ function love.load()
     currentPlayer = "white"
     validMoves = {}
 
+    borderSize = 40
+    uiHeight = borderSize * 2
     pieceMovedSound = love.audio.newSource("assets/sounds/pieceMoved.ogg", "static")
     pieceTakenSound = love.audio.newSource("assets/sounds/pieceTaken1.ogg", "static")
+    regularFont = love.graphics.newFont("assets/fonts/OpenDyslexic-Regular.otf", 14)
+    boldFont = love.graphics.newFont("assets/fonts/OpenDyslexic-Bold.otf", 14)
+    gameStartTime = love.timer.getTime()
+    turnStartTime = love.timer.getTime()
+    lastMove = ""
+    inCheck = false
 
     function love.mousepressed(x, y, button)
-        print("mousepressed(".. x ..", ".. y..", ".. button ..")")
         if button == 1 then
-            print("mousepressed(button == 1)")
-            local i = math.floor(y / 100) + 1
-            local j = math.floor(x / 100) + 1
-        print("mousepressed(".. i ..", ".. j ..")")
-        if selectedPiece then
-                print("mousepressed(selectedPiece)")
-                if isValidMove(i, j) then
-                    local targetPiece = pieces[i][j]
-                    pieces[i][j] = selectedPiece
-                    pieces[selectedX][selectedY] = ""
-                    selectedPiece = nil
-                    selectedX, selectedY = nil, nil
-                    validMoves = {}
-                    if targetPiece ~= "" then
-                        pieceTakenSound:setPitch(math.random(8, 32) / 16) -- Randomly shift pitch by an octave or two
-                        love.audio.play(pieceTakenSound)
+            local windowWidth, windowHeight = love.graphics.getDimensions()
+            local squareSize = (windowHeight - 2 * borderSize - 2 * uiHeight) / 8
+            local boardSize = squareSize * 8
+            local boardX = (windowWidth - boardSize) / 2
+            local boardY = (windowHeight - boardSize) / 2 + uiHeight / 2
+
+            local i = math.floor((y - boardY) / squareSize) + 1
+            local j = math.floor((x - boardX) / squareSize) + 1
+
+            if i >= 1 and i <= 8 and j >= 1 and j <= 8 then
+                if selectedPiece then
+                    if isValidMove(i, j) then
+                        local targetPiece = pieces[i][j]
+                        pieces[i][j] = selectedPiece
+                        pieces[selectedX][selectedY] = ""
+                        selectedPiece = nil
+                        selectedX, selectedY = nil, nil
+                        validMoves = {}
+                        if targetPiece ~= "" then
+                            pieceTakenSound:setPitch(math.random(8, 32) / 16) -- Randomly shift pitch by an octave or two
+                            love.audio.play(pieceTakenSound)
+                        else
+                            pieceMovedSound:setPitch(math.random(8, 32) / 16) -- Randomly shift pitch by an octave or two
+                            love.audio.play(pieceMovedSound)
+                        end
+                        switchPlayer()
+                    elseif selectedX == i and selectedY == j then
+                        selectedPiece = nil
+                        selectedX, selectedY = nil, nil
+                        validMoves = {}
+                    elseif pieces[i][j] ~= "" and getPieceColor(pieces[i][j]) == currentPlayer then
+                        selectedPiece = pieces[i][j]
+                        selectedX, selectedY = i, j
+                        validMoves = getValidMoves(i, j)
                     else
-                        pieceMovedSound:setPitch(math.random(8, 32) / 16) -- Randomly shift pitch by an octave or two
-                        love.audio.play(pieceMovedSound)
+                        selectedPiece = nil
+                        selectedX, selectedY = nil, nil
+                        validMoves = {}
                     end
-                    switchPlayer()
-                elseif selectedX == i and selectedY == j then
-                    selectedPiece = nil
-                    selectedX, selectedY = nil, nil
-                    validMoves = {}
                 elseif pieces[i][j] ~= "" and getPieceColor(pieces[i][j]) == currentPlayer then
                     selectedPiece = pieces[i][j]
                     selectedX, selectedY = i, j
@@ -60,26 +83,30 @@ function love.load()
                     selectedX, selectedY = nil, nil
                     validMoves = {}
                 end
-            elseif pieces[i][j] ~= "" and getPieceColor(pieces[i][j]) == currentPlayer then
-                selectedPiece = pieces[i][j]
-                selectedX, selectedY = i, j
-                validMoves = getValidMoves(i, j)
-            else
-                selectedPiece = nil
-                selectedX, selectedY = nil, nil
-                validMoves = {}
             end
         end
     end
 
     function love.mousemoved(x, y, dx, dy)
-        hoveredX = math.floor(y / 100) + 1
-        hoveredY = math.floor(x / 100) + 1
+        local windowWidth, windowHeight = love.graphics.getDimensions()
+        local squareSize = (windowHeight - 2 * borderSize - 2 * uiHeight) / 8
+        local boardSize = squareSize * 8
+        local boardX = (windowWidth - boardSize) / 2
+        local boardY = (windowHeight - boardSize) / 2 + uiHeight / 2
+
+        hoveredX = math.floor((y - boardY) / squareSize) + 1
+        hoveredY = math.floor((x - boardX) / squareSize) + 1
+
+        if hoveredX < 1 or hoveredX > 8 or hoveredY < 1 or hoveredY > 8 then
+            hoveredX, hoveredY = nil, nil
+        end
     end
 
     -- Example of changing colors using hex codes
     whiteColor = hexToRgb("#EBECD3")
     blackColor = hexToRgb("#7D945D")
+    backgroundColor = hexToRgb("#B4C098")
+    borderColor = hexToRgb("#453643")
 
     for i = 1, 8 do
         for j = 1, 8 do
@@ -114,13 +141,46 @@ function love.load()
 end
 
 function love.draw()
-    local squareSize = 100
+    local windowWidth, windowHeight = love.graphics.getDimensions()
+    local squareSize = (windowHeight - 2 * borderSize - 2 * uiHeight) / 8
+    local boardSize = squareSize * 8
+    local boardX = (windowWidth - boardSize) / 2
+    local boardY = (windowHeight - boardSize) / 2 + uiHeight / 2
+
+    -- Set background color to Poker green
+    love.graphics.clear(backgroundColor)
+
+    -- Draw the board
+    drawBoard(boardX, boardY, squareSize, boardSize)
+
+    -- Draw UI elements
+    drawUI(boardX, boardY, boardSize)
+end
+
+function drawBoard(boardX, boardY, squareSize, boardSize)
     local borderWidth = 5
     local cornerLength = 20
+
+    -- Draw oak border around the board
+    love.graphics.setColor(borderColor)
+    love.graphics.rectangle("fill", boardX - borderSize, boardY - borderSize, boardSize + 2 * borderSize, boardSize + 2 * borderSize)
+
+    -- Draw chess notation coordinates
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.setFont(regularFont)
+    for i = 1, 8 do
+        local letter = string.char(96 + i):upper()
+        local number = tostring(9 - i)
+        love.graphics.print(letter, boardX + (i - 1) * squareSize + squareSize / 2, boardY - borderSize / 2, 0, 1, 1, 6, 6)
+        love.graphics.print(letter, boardX + (i - 1) * squareSize + squareSize / 2, boardY + boardSize + borderSize / 2, 0, 1, 1, 6, 6)
+        love.graphics.print(number, boardX - borderSize / 2, boardY + (i - 1) * squareSize + squareSize / 2, 0, 1, 1, 6, 6)
+        love.graphics.print(number, boardX + boardSize + borderSize / 2, boardY + (i - 1) * squareSize + squareSize / 2, 0, 1, 1, 6, 6)
+    end
+
     for i = 1, 8 do
         for j = 1, 8 do
             love.graphics.setColor(board[i][j].color)
-            love.graphics.rectangle("fill", (j - 1) * squareSize, (i - 1) * squareSize, squareSize, squareSize)
+            love.graphics.rectangle("fill", boardX + (j - 1) * squareSize, boardY + (i - 1) * squareSize, squareSize, squareSize)
             if pieces[i][j] ~= "" then
                 local pieceImage = pieceImages[pieces[i][j]]
                 local pieceWidth = pieceImage:getWidth()
@@ -128,20 +188,20 @@ function love.draw()
                 local offsetX = (squareSize - pieceWidth) / 2
                 local offsetY = (squareSize - pieceHeight) / 2
                 love.graphics.setColor(1, 1, 1)
-                love.graphics.draw(pieceImage, (j - 1) * squareSize + offsetX, (i - 1) * squareSize + offsetY)
+                love.graphics.draw(pieceImage, boardX + (j - 1) * squareSize + offsetX, boardY + (i - 1) * squareSize + offsetY)
             end
         end
     end
 
     if hoveredX and hoveredY then
         love.graphics.setColor(1, 0.5, 0, 0.5) -- Translucent orange
-        love.graphics.rectangle("fill", (hoveredY - 1) * squareSize, (hoveredX - 1) * squareSize, squareSize, squareSize)
+        love.graphics.rectangle("fill", boardX + (hoveredY - 1) * squareSize, boardY + (hoveredX - 1) * squareSize, squareSize, squareSize)
     end
 
     if selectedX and selectedY then
         love.graphics.setColor(0, 0.75, 1) -- Sky blue
         love.graphics.setLineWidth(borderWidth)
-        love.graphics.rectangle("line", (selectedY - 1) * squareSize + borderWidth / 2, (selectedX - 1) * squareSize + borderWidth / 2, squareSize - borderWidth, squareSize - borderWidth)
+        love.graphics.rectangle("line", boardX + (selectedY - 1) * squareSize + borderWidth / 2, boardY + (selectedX - 1) * squareSize + borderWidth / 2, squareSize - borderWidth, squareSize - borderWidth)
     end
 
     for _, move in ipairs(validMoves) do
@@ -152,8 +212,39 @@ function love.draw()
             love.graphics.setColor(1, 0.75, 0) -- Amber
         end
         love.graphics.setLineWidth(borderWidth)
-        drawCornerLines((move[2] - 1) * squareSize + borderWidth / 2, (move[1] - 1) * squareSize + borderWidth / 2, squareSize - borderWidth, borderWidth, cornerLength)
+        drawCornerLines(boardX + (move[2] - 1) * squareSize + borderWidth / 2, boardY + (move[1] - 1) * squareSize + borderWidth / 2, squareSize - borderWidth, borderWidth, cornerLength)
     end
+end
+
+function drawUI(boardX, boardY, boardSize)
+    love.graphics.setFont(regularFont)
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.print("Game Time: " .. string.format("%.2f", love.timer.getTime() - gameStartTime), boardX, boardY - uiHeight + 10)
+    love.graphics.print("Turn Time: " .. string.format("%.2f", love.timer.getTime() - turnStartTime), boardX + 200, boardY - uiHeight + 10)
+    if inCheck then
+        love.graphics.setFont(boldFont)
+        love.graphics.setColor(1, 0, 0)
+        love.graphics.printf("CHECK", boardX, boardY - uiHeight + 10, boardSize, "center")
+    end
+
+    -- Draw resign button
+    local resignButtonWidth = 100
+    local resignButtonX = boardX + boardSize - resignButtonWidth
+    local resignButtonY = boardY + boardSize + borderSize / 2 + 30
+    love.graphics.setColor(0.8, 0.1, 0.1)
+    love.graphics.rectangle("fill", resignButtonX, resignButtonY, resignButtonWidth, 30)
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.printf("Resign", resignButtonX, resignButtonY + 7, resignButtonWidth, "center")
+
+    -- Draw last move
+    love.graphics.setFont(regularFont)
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.print("Last Move: " .. lastMove, boardX, boardY + boardSize + borderSize / 2 + 30)
+
+    -- Draw current player
+    love.graphics.setFont(regularFont)
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.print("Current Player: " .. currentPlayer, boardX, boardY + boardSize + borderSize / 2 + 60)
 end
 
 function drawCornerLines(x, y, size, width, length)
@@ -173,6 +264,7 @@ end
 
 function switchPlayer()
     currentPlayer = currentPlayer == "white" and "black" or "white"
+    turnStartTime = love.timer.getTime()
 end
 
 function isValidMove(i, j)
